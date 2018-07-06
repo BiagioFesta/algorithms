62ee7e3f4e979aa44d4e21321f790ee9ac7252c0
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2018-07-07 01:32:22 +0200

    binomial coefficient

---
+++ test/testDynamicProgramming.cpp
@@ -0,0 +1,35 @@
+/*
+          Copyright (C) 2018  Biagio Festa
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+#include <gtest/gtest.h>
+#include <dynamicProgramming.hpp>
+#include <tuple>
+#include <vector>
+
+namespace algorithms::test {
+
+TEST(dynamicProgramming, binomialCoefficient) {
+  static std::vector<std::tuple<int, int, int>> testCases = {
+      {4, 2, 6}, {1, 1, 1}, {20, 3, 1140}};
+
+  for (const auto& test : testCases) {
+    ASSERT_EQ(binomialCoefficient(std::get<0>(test), std::get<1>(test)),
+              std::get<2>(test));
+  }
+}
+
+}  // namespace algorithms::test
