05a95dc705cece18f8ec2c25e406e31bf34dd7a2
Biagio Festa <biagio.festa@gmail.com>
2018-08-04 00:39:31 +0200

    Implemented maximumGap algorithm

--- test/testAlgArrays.cpp
+++ test/testAlgArrays.cpp
@@ -147,4 +147,16 @@ TEST(AlgArrays, largestNumber) {
   }
 }
 
+TEST(AlgArrays, maximumGap) {
+  using Test = std::pair<std::vector<int>, int>;
+  const std::vector<Test> testCases = {{{3, 5, 4, 2}, 2},
+                                       {{1, 10}, 1},
+                                       {{-1, -1, -1, 2}, 3},
+                                       {{1, 2, 2, 2, 2}, 4}};
+
+  for (const auto& [v, expt] : testCases) {
+    ASSERT_EQ(maximumGap(v), expt);
+  }
+}
+
 }  // namespace algorithms::test
