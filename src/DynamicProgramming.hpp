ac315ba0bc020282e6edb1c1e988a41a26cf297f
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2018-11-15 00:37:05 +0100

    Implemented twoEggsDropping algorithm

--- src/DynamicProgramming.hpp
+++ src/DynamicProgramming.hpp
@@ -127,6 +127,37 @@ int maxProfit(const std::vector<int>& iPrices);
 std::size_t findRotateSteps(const std::string_view iRing,
                             const std::string_view iKey);
 
+/*! \brief The following is a description of the instance of this famous puzzle
+ *  involving 2 eggs and a building with `iNumFloors` floors.
+ *
+ *  Suppose that we wish to know which stories in a `iNumFloots`-story building
+ *  are safe to drop eggs from, and which will cause the eggs to break on
+ *  landing. What strategy should be used to drop eggs such that total number of
+ *  drops in worst case is minimized and we find the required floor.
+ *
+ *  We may make a few assumptions:
+ *      - An egg that survives a fall can be used again.
+ *      - A broken egg must be discarded.
+ *      - The effect of a fall is the same for all eggs.
+ *      - If an egg breaks when dropped, then it would break if dropped from a
+ *        higher floor.
+ *      - If an egg survives a fall then it would survive a shorter fall.
+ *
+ *  \Complexity      Time: O(N^2)      Space: O(N)
+ *  \url https://www.geeksforgeeks.org/puzzle-set-35-2-eggs-and-100-floors/
+ *
+ *  \return a pair. The first is the number of attempts of the worst case of the
+ *  optimal strategy required in order to find the floor.
+ *  The second is the index (0-based) of the floor where the first attempt
+ *  should be performed in the optimal strategy.
+ *
+ *  \note You can compute the entire sequence of floors to select in the optimal
+ *  strategy (considering the worst case) calling this function recursively on a
+ *  remaing number of floors having the first attempt.
+ *  \note `iNumFloors` has to be a non-negative integer.
+ */
+std::pair<int, int> twoEggsDropping(const int iNumFloors);
+
 }  // namespace algorithms
 
 #endif  // ALGORITHMS__DYNAMIC_PROGRAMMING__HPP
