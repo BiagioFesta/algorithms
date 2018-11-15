c4911c03961f2b0df85cf5e1d1b4efe91fe4b1f9
Biagio Festa <biagio.festa@gmail.com>
2018-11-15 21:26:00 +0100

    Implemented cheapestJump algorithm

--- test/testDynamicProgramming.cpp
+++ test/testDynamicProgramming.cpp
@@ -136,4 +136,15 @@ TEST(dynamicProgramming, twoEggsDropping) {
   }
 }
 
+TEST(dynamicProgramming, cheapestJump) {
+  using Test = std::tuple<std::vector<int>, int, std::vector<int>>;
+  const std::vector<Test> testCases = {{{1, 2, 3, 4}, 0, {}},
+                                       {{1, 2, 4, -1, 2}, 2, {1, 3, 5}},
+                                       {{1, 2, 4, -1, 2}, 1, {}}};
+
+  for (const auto& [vect, jump, expt] : testCases) {
+    ASSERT_EQ(cheapestJump(vect, jump), expt);
+  }
+}
+
 }  // namespace algorithms::test
