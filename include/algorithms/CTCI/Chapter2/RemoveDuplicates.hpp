54dd1203c881eadf485da48a0e3108aede6c9570
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2018-12-16 01:03:39 +0100

    Implemented RemoveDuplicates algorithm

---
+++ include/algorithms/CTCI/Chapter2/RemoveDuplicates.hpp
@@ -0,0 +1,36 @@
+/*
+  Copyright (C) 2018  Biagio Festa
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#ifndef ALGORITHMS__CTCI__CHAPTER_2__NO_DUPLICATES__HPP
+#define ALGORITHMS__CTCI__CHAPTER_2__NO_DUPLICATES__HPP
+#include <list>
+
+namespace algorithms {
+
+/*! \brief Write code to remove duplicates from an unsorted linked list.
+ *  \note   Complexity  -> Time: O(N)    Space: O(N)
+ */
+void RemoveDuplicates(std::list<int>* ioList);
+
+/*! \brief Write code to remove duplicates from an unsorted linked list.
+ *  \note   Complexity  -> Time: O(N^2)    Space: O(1)
+ */
+void RemoveDuplicatesSpaceOptimized(std::list<int>* ioList);
+
+}  // namespace algorithms
+
+#endif  // ALGORITHMS__CTCI__CHAPTER_2__NO_DUPLICATES__HPP
---
+++ src/CTCI/Chapter2/RemoveDuplicates.cpp
@@ -0,0 +1,59 @@
+/*
+  Copyright (C) 2018  Biagio Festa
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#include <algorithms/CTCI/Chapter2/RemoveDuplicates.hpp>
+#include <iterator>
+#include <list>
+#include <type_traits>
+#include <unordered_set>
+
+namespace algorithms {
+
+void RemoveDuplicates(std::list<int>* ioList) {
+  std::unordered_set<int> hashTable;
+
+  auto iterator = ioList->begin();
+  while (iterator != ioList->end()) {
+    if (hashTable.insert(*iterator).second) {
+      ++iterator;
+    } else {
+      iterator = ioList->erase(iterator);
+    }
+  }
+}
+
+void RemoveDuplicatesSpaceOptimized(std::list<int>* ioList) {
+  using Iterator = decltype(ioList->begin());
+  Iterator iterator, successive;
+
+  iterator = ioList->begin();
+  while (iterator != ioList->end()) {
+    successive = std::next(iterator);
+
+    while (successive != ioList->end()) {
+      if (*iterator == *successive) {
+        successive = ioList->erase(successive);
+      } else {
+        ++successive;
+      }
+    }
+
+    ++iterator;
+  }
+}
+
+}  // namespace algorithms
