09000f5a985e166c3ce37b9c448c603eda0d2625
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2018-12-24 17:44:47 +0100

    Implemented GlobalLocalInversions algorithm

---
+++ include/algorithms/Array/GlobalLocalInversions.hpp
@@ -0,0 +1,40 @@
+/*
+  Copyright (C) 2018  Biagio Festa
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#ifndef ALGORITHMS__ARRAY__GLOBAL_LOCAL_INVERSIONS__HPP
+#define ALGORITHMS__ARRAY__GLOBAL_LOCAL_INVERSIONS__HPP
+#include <vector>
+
+namespace algorithms {
+
+/*! \brief We have some permutation A of [0, 1, ..., N - 1], where N is the
+ *  length of A.
+ *  The number of (global) inversions is the number of i < j with
+ *  0 <= i < j < N and A[i] > A[j].
+ *  The number of local inversions is the number of i with 0 <= i < N
+ *  and A[i] > A[i+1].
+ *  Return true if and only if the number of global inversions is equal to the
+ *  number of local inversions.
+ *
+ *  \note   Complexity  ->  Time: O(N)  Space: O(1)
+ *  \note https://leetcode.com/problems/global-and-local-inversions/
+ */
+bool GlobalLocalInversions(const std::vector<int>& iVector);
+
+}  // namespace algorithms
+
+#endif  // ALGORITHMS__ARRAY__GLOBAL_LOCAL_INVERSIONS__HPP
---
+++ src/Array/GlobalLocalInversions.cpp
@@ -0,0 +1,52 @@
+/*
+  Copyright (C) 2018  Biagio Festa
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#include <algorithm>
+#include <algorithms/Array/GlobalLocalInversions.hpp>
+#include <cassert>
+#include <cstdlib>
+#include <numeric>
+#include <vector>
+
+namespace {
+
+bool isValidVector(const std::vector<int>& iVector) {
+  std::vector<int> aNaturalSequence(iVector.size());
+  std::iota(aNaturalSequence.begin(), aNaturalSequence.end(), 0);
+
+  return std::is_permutation(
+      iVector.cbegin(), iVector.cend(), aNaturalSequence.cbegin());
+}
+
+}  // anonymous namespace
+
+namespace algorithms {
+
+bool GlobalLocalInversions(const std::vector<int>& iVector) {
+  assert(isValidVector(iVector));
+
+  const int kSize = static_cast<int>(iVector.size());
+  for (int i = 0; i < kSize; ++i) {
+    assert(iVector[i] >= 0 && iVector[i] < kSize);
+
+    const int diff = std::abs(i - iVector[i]);
+    if (diff > 1) return false;
+  }
+  return true;
+}
+
+}  // namespace algorithms
