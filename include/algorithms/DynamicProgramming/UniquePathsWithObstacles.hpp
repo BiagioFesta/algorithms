a043cd96cf6c7ad2c38cc323fb3d4384673fbcc0
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2019-01-16 01:10:55 +0100

    Implemented UniquePathsWithObstacles algorithm

---
+++ include/algorithms/DynamicProgramming/UniquePathsWithObstacles.hpp
@@ -0,0 +1,39 @@
+/*
+  Copyright (C) 2019  Biagio Festa
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#ifndef ALGORITHMS__DYNAMIC_PROGRAMMING__UNIQUE_PATHS_II__HPP
+#define ALGORITHMS__DYNAMIC_PROGRAMMING__UNIQUE_PATHS_II__HPP
+#include <vector>
+
+namespace algorithms {
+
+/*! \brief A robot is located at the top-left corner of a m x n grid.
+ *  The robot can only move either down or right at any point in time. The robot
+ *  is trying to reach the bottom-right corner of the grid.
+ *  Now consider if some obstacles are added to the grids. How many unique paths
+ *  would there be?
+ *  An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+ *
+ *  \note Complexity -> Time: O(N x M)   Space: O(N x M)
+ *  \note https://leetcode.com/problems/unique-paths-ii/
+ */
+int UniquePathsWithObstacles(
+    const std::vector<std::vector<int>>& iObstacleGrid);
+
+}  // namespace algorithms
+
+#endif  // ALGORITHMS__DYNAMIC_PROGRAMMING__UNIQUE_PATHS_II__HPP
---
+++ src/DynamicProgramming/UniquePathsWithObstacles.cpp
@@ -0,0 +1,54 @@
+/*
+  Copyright (C) 2019  Biagio Festa
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#include <algorithms/DynamicProgramming/UniquePathsWithObstacles.hpp>
+#include <cassert>
+#include <vector>
+
+namespace algorithms {
+
+int UniquePathsWithObstacles(
+    const std::vector<std::vector<int>>& iObstacleGrid) {
+  const int n = static_cast<int>(iObstacleGrid.size());
+  if (n == 0) return 0;
+  const int m = static_cast<int>(iObstacleGrid[0].size());
+  if (m == 0) return 0;
+
+  const int size = m * n;
+  std::vector<int> table(size);
+  if (!iObstacleGrid[n - 1][m - 1]) {
+    table.back() = 1;
+  }
+
+  for (int i = n - 1; i >= 0; --i) {
+    assert(static_cast<int>(iObstacleGrid[i].size()) == m);
+    for (int j = m - 1; j >= 0; --j) {
+      if (!iObstacleGrid[i][j]) {
+        if (i != n - 1) {
+          table[i * m + j] += table[(i + 1) * m + j];
+        }
+        if (j != m - 1) {
+          table[i * m + j] += table[i * m + j + 1];
+        }
+      }
+    }
+  }
+
+  return table[0];
+}
+
+}  // namespace algorithms
