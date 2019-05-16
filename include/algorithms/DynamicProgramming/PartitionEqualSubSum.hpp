924546e075b76be5105d1c279e6d3fc0f0e214d2
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2019-05-16 13:01:51 +0100

    Implemented PartitionEqualSubSum algorithm

---
+++ include/algorithms/DynamicProgramming/PartitionEqualSubSum.hpp
@@ -0,0 +1,37 @@
+/*
+  Copyright (C) 2019  Biagio Festa
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#ifndef ALGORITHMS__DYNAMIC_PROGRAMMING__PARTITION_EQUAL_SUB_SUM__HPP
+#define ALGORITHMS__DYNAMIC_PROGRAMMING__PARTITION_EQUAL_SUB_SUM__HPP
+#include <vector>
+
+namespace algorithms {
+
+/*! \brief Given a non-empty array containing only positive integers, find if
+ *  the array can be partitioned into two subsets such that the sum of elements
+ *  in both subsets is equal.
+ *  \note Each of the array element will not exceed 100.
+ *  \note The array size will not exceed 200.
+ *
+ *  \note Complexity -> Time: O(N x M)  Space: O(N x M)    where M is the sum.
+ *  \note https://leetcode.com/problems/partition-equal-subset-sum/
+ */
+bool PartitionEqualSubSum(const std::vector<int>& nums);
+
+}  // namespace algorithms
+
+#endif  // ALGORITHMS__DYNAMIC_PROGRAMMING__PARTITION_EQUAL_SUB_SUM__HPP
---
+++ src/DynamicProgramming/PartitionEqualSubSum.cpp
@@ -0,0 +1,55 @@
+/*
+  Copyright (C) 2019  Biagio Festa
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#include <algorithms/DynamicProgramming/PartitionEqualSubSum.hpp>
+#include <cassert>
+#include <numeric>
+#include <vector>
+
+namespace algorithms {
+
+bool PartitionEqualSubSum(const std::vector<int>& nums) {
+  constexpr int kMaxValue = 100;
+  constexpr int kMaxSize = 200;
+
+  const auto kSum = std::accumulate(nums.cbegin(), nums.cend(), 0);
+  if (kSum & 0x1 || kSum == 0) {
+    return false;
+  }
+  const int kSize = nums.size();
+  assert(kSize <= kMaxSize);
+  const int kHalfSum = kSum >> 1;
+  const int kM = kHalfSum + 1;
+  const int kN = kSize + 1;
+
+  // N x M
+  std::vector<bool> table(kN * kM, false);
+  table[kSize * kM + kHalfSum] = true;
+
+  for (int i = kSize - 1; i >= 0; --i) {
+    assert(nums[i] <= kMaxValue);
+    for (int j = 0; j < kM; ++j) {
+      table[i * kM + j] =
+          table[(i + 1) * kM + j] ||
+          (j + nums[i] < kM ? table[(i + 1) * kM + j + nums[i]] : false);
+    }
+  }
+
+  return table[0];
+}
+
+}  // namespace algorithms
