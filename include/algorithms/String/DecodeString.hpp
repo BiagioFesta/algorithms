0b3eabe732a60d601c2b9c47d0e44699fe49a546
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2019-05-21 17:13:18 +0100

    Implemented DecodeString algorithm

---
+++ include/algorithms/String/DecodeString.hpp
@@ -0,0 +1,41 @@
+/*
+  Copyright (C) 2019  Biagio Festa
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#ifndef ALGORITHMS__STRING__DECODE_STRING__HPP
+#define ALGORITHMS__STRING__DECODE_STRING__HPP
+#include <string>
+
+namespace algorithms {
+
+/*! \brief  Given an encoded string, return it's decoded string.
+ *  The encoding rule is: k[encoded_string], where the encoded_string inside the
+ *  square brackets is being repeated exactly k times.
+ *  Note that k is guaranteed to be a positive integer.
+ *  You may assume that the input string is always valid; No extra white spaces,
+ *  square brackets are well-formed, etc.
+ *  Furthermore, you may assume that the original data does not contain any
+ *  digits and that digits are only for those repeat numbers, k. For example,
+ *  there won't be input like 3a or 2[4].
+ *
+ *  \note  Complexity:  Time: O(N)    Space: O(N)
+ *  \note https://leetcode.com/problems/decode-string/
+ */
+std::string DecodeString(const std::string &encStr);
+
+}  // namespace algorithms
+
+#endif  // ALGORITHMS__STRING__DECODE_STRING__HPP
---
+++ src/String/DecodeString.cpp
@@ -0,0 +1,57 @@
+/*
+  Copyright (C) 2019  Biagio Festa
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#include <algorithms/String/DecodeString.hpp>
+#include <cassert>
+#include <string>
+
+namespace algorithms {
+
+std::string DecodeString(const std::string& encStr) {
+  std::string decStr;
+  std::size_t index = 0;
+
+  while (index < encStr.size()) {
+    const char c = encStr[index];
+    if (c >= '0' && c <= '9') {
+      const std::size_t openBkIndex = encStr.find('[', index);
+      assert(openBkIndex != std::string::npos);
+      const int rep = std::stoi(encStr.substr(index, openBkIndex - index));
+      index = openBkIndex + 1;
+      for (std::size_t bkCount = 1; bkCount; ++index) {
+        const char p = encStr[index];
+        if (p == '[') {
+          ++bkCount;
+        } else if (p == ']') {
+          --bkCount;
+        }
+      }
+      const auto innerDecoded =
+          DecodeString(encStr.substr(openBkIndex + 1, index - openBkIndex - 2));
+      for (int i = 0; i < rep; ++i) {
+        decStr += innerDecoded;
+      }
+    } else {
+      decStr += c;
+      ++index;
+    }
+  }
+
+  return decStr;
+}
+
+}  // namespace algorithms
