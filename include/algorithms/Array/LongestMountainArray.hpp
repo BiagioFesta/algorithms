fdb90289c2a820a8a07ba7cf52c4f64a16b7409e
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2019-07-17 12:57:48 +0100

    Implemented LongestMountainArray algorithm

---
+++ include/algorithms/Array/LongestMountainArray.hpp
@@ -0,0 +1,40 @@
+/*
+  Copyright (C) 2019  Biagio Festa
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#ifndef ALGORITHMS__ARRAY__LONGESTMOUNTAINARRAY__HPP
+#define ALGORITHMS__ARRAY__LONGESTMOUNTAINARRAY__HPP
+#include <vector>
+
+namespace algorithms {
+
+/*! \brief Let's call any (contiguous) subarray B (of A) a mountain if the
+ *  following properties hold:
+ *     - B.length >= 3
+ *     - There exists some 0 < i < B.length - 1 such that
+ *       B[0] < B[1] < ... B[i-1] < B[i] > B[i+1] > ... > B[B.length - 1]
+ *  (Note that B could be any subarray of A, including the entire array A.)
+ *  Given an array A of integers, return the length of the longest mountain.
+ *  Return 0 if there is no mountain.
+ *
+ *  \note  Complexity  -->  Time: O(N)  Space: O(1)
+ *  \note https://leetcode.com/problems/longest-mountain-in-array/
+ */
+int LongestMountainArray(const std::vector<int>& A);
+
+}  // namespace algorithms
+
+#endif  // ALGORITHMS__ARRAY__LONGESTMOUNTAINARRAY__HPP
---
+++ src/Array/LongestMountainArray.cpp
@@ -0,0 +1,44 @@
+/*
+  Copyright (C) 2019  Biagio Festa
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#include <algorithm>
+#include <algorithms/Array/LongestMountainArray.hpp>
+#include <vector>
+
+namespace algorithms {
+
+int LongestMountainArray(const std::vector<int>& A) {
+  const int size = A.size();
+  int i, j, max = 0;
+
+  i = 0;
+  while (i < size - 2) {
+    while (i < size - 1 && A[i + 1] <= A[i]) ++i;
+    j = i + 1;
+    while (j < size && A[j - 1] < A[j]) ++j;
+    if (j < size && A[j - 1] != A[j]) {
+      while (j < size && A[j] < A[j - 1]) ++j;
+      max = std::max(max, j - i);
+    }
+
+    i = j - 1;
+  }
+
+  return max;
+}
+
+}  // namespace algorithms
