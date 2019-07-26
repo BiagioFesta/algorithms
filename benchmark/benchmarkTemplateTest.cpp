26e1f32dff6a9b7bbe5ce80e5975dcd391c62c6b
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2019-07-26 19:22:48 +0100

    Added template benchmark for generic algorithm benchmarks

---
+++ benchmark/benchmarkTemplateTest.cpp
@@ -0,0 +1,37 @@
+/*
+  Copyright (C) 2019  Biagio Festa
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#include <benchmark/benchmark.h>
+#include <algorithms/String/FindAllAnagramsString.hpp>
+
+/*! \brief Macro generate a benchmark for a specific algorithm.
+ *  \param [in] FunctionName  The function name implementing the algorithm.
+ *  \param [in] ...           The args to pass to the function (input data).
+ */
+#define GENERATE_BENCHMARK(FunctionName, ...)        \
+  void BM##FunctionName(::benchmark::State& state) { \
+    for (auto _ : state) {                           \
+      ::algorithms::FunctionName(__VA_ARGS__);       \
+    }                                                \
+  }                                                  \
+  BENCHMARK(BM##FunctionName)
+
+namespace algorithms::benchmark {
+
+GENERATE_BENCHMARK(FindAllAnagramsString, "cbaebabacd", "abc");
+
+}  // namespace algorithms::benchmark
