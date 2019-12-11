34af5e6969156d121424139571662ffb0afaf230
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2019-12-11 13:33:32 +0000

    Fixed hacker_rank HTTP request for parsing

--- docs/GenTable/src/parser.py
+++ docs/GenTable/src/parser.py
@@ -158,7 +158,7 @@ class Parser:
 
     @staticmethod
     def __get_meta_hacker_rank(url):
-        response = requests.get(url)
+        response = requests.get(url, headers={'User-Agent': None})
         if response.status_code != 200:
             raise Exception('Cannot parse metadata for url "{}" (Get Failed)'
                             .format(url))
