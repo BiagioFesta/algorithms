8d8976780ec296df7349722b72a63cef4a3db13d
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2019-12-16 16:18:08 +0000

    Implemented LongestArithmeticSubsequenceDifference algorithm

---
+++ include/algorithms/DynamicProgramming/LongestArithmeticSubsequenceDifference.hpp
@@ -0,0 +1,38 @@
+/*
+  Copyright (C) 2019  Biagio Festa
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#ifndef ALGORITHMS__DYNAMICPROGRAMMING__LONGESTARITHMETICSUBSEQUENCEDIFFERENCE__HPP
+#define ALGORITHMS__DYNAMICPROGRAMMING__LONGESTARITHMETICSUBSEQUENCEDIFFERENCE__HPP
+#include <vector>
+
+namespace algorithms {
+
+/*! \brief Given an integer array arr and an integer difference, return the
+ *  length of the longest subsequence in arr which is an arithmetic sequence
+ *  such that the difference between adjacent elements in the subsequence equals
+ *  difference.
+ *
+ *  \note  Complexity  -->  Time: O(N)  Space: O(N)
+ *  \note
+ * https://leetcode.com/problems/longest-arithmetic-subsequence-of-given-difference/
+ */
+int LongestArithmeticSubsequenceDifference(const std::vector<int>& arr,
+                                           const int difference);
+
+}  // namespace algorithms
+
+#endif  // ALGORITHMS__DYNAMICPROGRAMMING__LONGESTARITHMETICSUBSEQUENCEDIFFERENCE__HPP
---
+++ test/DynamicProgramming/testLongestArithmeticSubsequenceDifference.cpp
@@ -0,0 +1,36 @@
+/*
+  Copyright (C) 2019  Biagio Festa
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#include <gtest/gtest.h>
+#include <algorithms/DynamicProgramming/LongestArithmeticSubsequenceDifference.hpp>
+#include <tuple>
+#include <vector>
+
+namespace algorithms::test {
+
+TEST(DynamicProgramming, LongestArithmeticSubsequenceDifference) {
+  using Test = std::tuple<std::vector<int>, int, int>;
+  const std::vector<Test> testCases = {{{1, 5, 7, 8, 5, 3, 4, 2, 1}, -2, 4},
+                                       {{1, 2, 3, 4}, 1, 4},
+                                       {{1, 3, 5, 7}, 1, 1}};
+
+  for (const auto& [arr, difference, expt] : testCases) {
+    ASSERT_EQ(LongestArithmeticSubsequenceDifference(arr, difference), expt);
+  }
+}
+
+}  // namespace algorithms::test
