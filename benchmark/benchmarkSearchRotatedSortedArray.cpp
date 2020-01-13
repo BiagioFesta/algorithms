7a91e266bb0fbf44e474c2a784e5c9586b2c3c1e
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2020-01-13 15:51:42 +0000

    Updated copyright year

--- benchmark/benchmarkSearchRotatedSortedArray.cpp
+++ benchmark/benchmarkSearchRotatedSortedArray.cpp
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2019  Biagio Festa
+  Copyright (C) 2020  Biagio Festa
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
